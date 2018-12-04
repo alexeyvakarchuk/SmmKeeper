099663d2b67242bcd797d3a9a6afecfd32339db7
Unknown <mrakobes10@mail.ru>
2018-12-04 19:29:02 +0200

    Redesign profile-info block

--- views/sections/InstaProfilePage/index.js
+++ views/sections/InstaProfilePage/index.js
@@ -19,15 +19,50 @@ class InstaProfilePage extends PureComponent<Props, State> {
   }
 
   render() {
-    const { username, tasksList } = this.props;
+    const { username, tasksList, accList } = this.props;
+
+    const acc = this.props.accList.find(el => el.username === username);
+
     return (
       <div className="instaprofile">
         {/* <h1 className="instaprofile__name">@{username}</h1> */}
         <div className="container-fluid">
           <div className="row">
             <div className="col-lg-2 col-md-3">
-              <div className="panel panel-info">
-                <div className="panel-info__name">{username}</div>
+              <div className="panel profile-info">
+                <div className="profile-info__profile">
+                  <span className="profile-info__profile-text">Profile</span>
+                  <span className="profile-info__profile-text profile-info__profile-text_grey">
+                    Paid till {`27.10.2019`}
+                  </span>
+                </div>
+                <span className="profile-info__name">{username}</span>
+                <div className="profile-info__block">
+                  <span className="profile-info__text">
+                    {acc.stats[0].followers}
+                  </span>
+                  <span className="profile-info__text profile-info__text_mark">
+                    {acc.stats.length > 1
+                      ? acc.stats[0].followers - acc.stats[1].followers
+                      : ""}
+                  </span>
+                  <span className="profile-info__caption">Followers</span>
+                </div>
+                <div className="profile-info__block">
+                  <span className="profile-info__text">
+                    {acc.stats[0].follows}
+                  </span>
+                  <span className="profile-info__text profile-info__text_mark">
+                    {acc.stats.length > 1
+                      ? acc.stats[0].follows - acc.stats[1].follows
+                      : ""}
+                  </span>
+                  <span className="profile-info__caption">Following</span>
+                </div>
+                <div className="profile-info__block">
+                  <span className="profile-info__text">{65}</span>
+                  <span className="profile-info__caption">Posts</span>
+                </div>
               </div>
             </div>
             <div className="col-lg-4 col-md-3">
@@ -68,6 +103,7 @@ class InstaProfilePage extends PureComponent<Props, State> {
   }
 }
 
-export default connect(({ inst: { tasksList } }) => ({
+export default connect(({ inst: { accList, tasksList } }) => ({
+  accList,
   tasksList
 }))(InstaProfilePage);
