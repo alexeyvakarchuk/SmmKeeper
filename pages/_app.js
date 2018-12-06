5fd1a78bd1415377e21eefaa5215bcf15e42897d
Alexey <alexeyvakarchuk@yandex.ru>
2018-12-06 21:40:13 +0200

    Add service workers for all the pages

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,19 @@ class MyApp extends App {
     };
   }
 
+  componentDidMount() {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then(registration => {
+          console.log("service worker registration successful");
+        })
+        .catch(err => {
+          console.warn("service worker registration failed", err.message);
+        });
+    }
+  }
+
   render() {
     const { Component, pageProps, store } = this.props;
 
