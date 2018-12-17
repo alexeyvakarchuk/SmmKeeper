81d76aebabcfdc88ca18738c4226aabc7abf7e81
Alexey <alexeyvakarchuk@yandex.ru>
2018-12-17 18:03:32 +0200

    Remove service worker registration for the dev environment

--- pages/_app.js
+++ pages/_app.js
@@ -18,36 +18,40 @@ class MyApp extends App {
   }
 
   componentDidMount() {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/service-worker.js")
-        .then(registration => {
-          console.log("service worker registration successful");
-        })
-        .catch(err => {
-          console.warn("service worker registration failed", err.message);
+    const environment = process.env.NODE_ENV;
+
+    if (environment === "production") {
+      if ("serviceWorker" in navigator) {
+        navigator.serviceWorker
+          .register("/service-worker.js")
+          .then(registration => {
+            console.log("service worker registration successful");
+          })
+          .catch(err => {
+            console.warn("service worker registration failed", err.message);
+          });
+      }
+
+      let deferredPrompt;
+
+      window.addEventListener("beforeinstallprompt", e => {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+
+        deferredPrompt.prompt();
+
+        deferredPrompt.userChoice.then(choiceResult => {
+          if (choiceResult.outcome === "accepted") {
+            console.log("User accepted the A2HS prompt");
+          } else {
+            console.log("User dismissed the A2HS prompt");
+          }
+          deferredPrompt = null;
         });
-    }
-
-    let deferredPrompt;
-
-    window.addEventListener("beforeinstallprompt", e => {
-      // Prevent Chrome 67 and earlier from automatically showing the prompt
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      deferredPrompt = e;
-
-      deferredPrompt.prompt();
-
-      deferredPrompt.userChoice.then(choiceResult => {
-        if (choiceResult.outcome === "accepted") {
-          console.log("User accepted the A2HS prompt");
-        } else {
-          console.log("User dismissed the A2HS prompt");
-        }
-        deferredPrompt = null;
       });
-    });
+    }
   }
 
   render() {
