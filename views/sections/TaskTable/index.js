63ecfbeaa90240234e2169b748bd27c74a988e43
Vlad <mrakobes10@mail.ru>
2018-12-25 21:33:54 +0200

    End filter block

--- views/sections/TaskTable/index.js
+++ views/sections/TaskTable/index.js
@@ -14,23 +14,45 @@ export default class TaskTable extends Component {
   render() {
     const { selectedOption } = this.state;
 
-    const options = [
+    const optionsAction = [
       { value: "chocolate", label: "Chocolate" },
       { value: "strawberry", label: "Strawberry" },
       { value: "vanilla", label: "Vanilla" }
+    ];
+    const optionsFilter = [
+      { value: "Follow", label: "Follow" },
+      { value: "Follow & Like", label: "Follow & Like" },
+      { value: "Unfollow", label: "Unfollow" },
+      { value: "Like", label: "Like" },
+      { value: "Direct", label: "Direct" }
     ];
 
     return (
       <section className="task-table">
         <div className="task-table__filter">
-          <Dropdown
-            instanceId="task-select"
-            value={selectedOption}
-            onChange={this.handleChange}
-            options={options}
-            placeholder={"Bulk action"}
-          />
-          <button className="btn-task">Add new task</button>
+          <div className="task-table__filter-left">
+            <Dropdown
+              instanceId="task-select"
+              value={selectedOption}
+              onChange={this.handleChange}
+              options={optionsAction}
+              placeholder={"Bulk action"}
+            />
+            <button className="btn-task btn-task_light btn-task_ml10">
+              Apply
+            </button>
+          </div>
+          <div className="task-table__filter-right">
+            <span className="task-table__filter-caption">Filter:</span>
+            <Dropdown
+              instanceId="task-select2"
+              value={selectedOption}
+              onChange={this.handleChange}
+              options={optionsFilter}
+              placeholder={"All actions"}
+            />
+            <button className="btn-task btn-task_ml20">Add new task</button>
+          </div>
         </div>
         <table className="task-table__table table">
           <thead className="table__thead">
