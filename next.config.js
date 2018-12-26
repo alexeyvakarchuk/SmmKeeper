c3f41e917d8ee28ed471c92d0f3261d0ec52b6b7
Alexey <alexeyvakarchuk@yandex.ru>
2018-12-26 16:14:11 +0200

    Add sourcemaps  for sass

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const withSass = require("@zeit/next-sass");
 const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
 
 module.exports = withSass({
+  sassLoaderOptions: {
+    sourceMap: true
+  },
+  postcssLoaderOptions: {
+    sourceMap: true
+  },
   webpack: config => {
     config.plugins.push(
       new SWPrecacheWebpackPlugin({
