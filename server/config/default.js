7d54951f46e532154722cd532109fc2693eb6bec
Alexey <alexeyvakarchuk@yandex.ru>
2019-02-10 17:04:56 +0200

    Env port fix

--- server/config/default.js
+++ server/config/default.js
@@ -21,7 +21,7 @@ module.exports = {
       };
     }
   },
-  port: process.env.PORT || 3004,
+  port: process.env.NODE_ENV === "production" ? 3000 : 3004,
   // get domain() {
   //   return `http://localhost:${this.port}`;
   // },
